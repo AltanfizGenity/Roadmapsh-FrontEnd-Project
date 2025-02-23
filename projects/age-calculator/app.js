ee4ca4aa994de733ba62b833c6f758a37a1c9397
Altanfiz <altanfizgenity@gmail.com>
2025-02-23 10:20:27 +0700

    add input validation for age form

--- projects/age-calculator/app.js
+++ projects/age-calculator/app.js
@@ -4,10 +4,22 @@ let ageForm = document.querySelector(".age-calculator");
 let resultElement = document.querySelector(".result");
 
 function calculateAge(userBirthday) {
+  if (!userBirthday) {
+    throw Error("Birthday params can't be empty");
+  }
+
   const now = DateTime.now();
   const birthday = DateTime.fromISO(userBirthday);
-  const diff = now.diff(birthday, ["year", "month", "day"]).toObject();
 
+  if (!birthday.isValid) {
+    throw Error("Invalid birthday date format");
+  }
+
+  if (birthday > now) {
+    throw Error("Cannot calculate future birthday date");
+  }
+
+  const diff = now.diff(birthday, ["year", "month", "day"]).toObject();
   return diff;
 }
 
