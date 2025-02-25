7b0be24d7e150859cac1e7257ceb1ef1a671b95f
Altanfiz <altanfizgenity@gmail.com>
2025-02-25 17:15:45 +0700

    create template component

---
+++ projects/flash-card/src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  import Card from "./libs/Card.svelte";
+  import Controller from "./libs/Controller.svelte";
+  import ProgressBar from "./libs/ProgressBar.svelte";
+</script>
+
+<main id="flash-card">
+    <ProgressBar/>
+    <Card/>
+    <Controller/>
+</main>
