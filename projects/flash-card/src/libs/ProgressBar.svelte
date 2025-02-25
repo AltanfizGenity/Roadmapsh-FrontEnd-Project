741fb2a5b36710fe2850de607c903bd69c66e0cc
Altanfiz <altanfizgenity@gmail.com>
2025-02-25 17:24:55 +0700

    create base layout for ProgressBar

--- projects/flash-card/src/libs/ProgressBar.svelte
+++ projects/flash-card/src/libs/ProgressBar.svelte
@@ -1,1 +1,7 @@
-<h1>progress bar</h1>
+<div class="progress-bar">
+  <div class="progress">
+    <div class="progress-glow"></div>
+    <div class="progress-rect"></div>
+  </div>
+  <div class="indicator">0 of 0</div>
+</div>
