b68bebdb0f5aba5231f96f6896d09883297961dd
Altanfiz <altanfizgenity@gmail.com>
2025-02-26 10:38:34 +0700

    style ProgressBar

--- projects/flash-card/src/libs/ProgressBar.svelte
+++ projects/flash-card/src/libs/ProgressBar.svelte
@@ -5,3 +5,48 @@
   </div>
   <div class="indicator">0 of 0</div>
 </div>
+
+<style>
+  .progress-bar {
+    position: relative;
+    width: 100%;
+    height: 48px;
+    border: 1px solid #000;
+    border-radius: var(--radius);
+    background-color: #fff;
+  }
+
+  .progress {
+    position: relative;
+    width: 100%;
+    height: 100%;
+    border-radius: inherit;
+
+    .progress-rect {
+      width: 100%;
+      height: 100%;
+      background-color: var(--accent);
+      border-radius: inherit;
+    }
+
+    .progress-glow {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: var(--accent);
+      border-radius: inherit;
+      filter: blur(25px);
+      z-index: 100;
+    }
+  }
+
+  .indicator {
+    position: absolute;
+    top: 50%;
+    right: 2.5%;
+    translate: 0 -50%;
+    z-index: 100;
+  }
+</style>
