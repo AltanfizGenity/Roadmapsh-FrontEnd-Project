8e4c826220fff02d65de36ab9d18223077c889a3
Altanfiz <altanfizgenity@gmail.com>
2025-02-26 21:13:26 +0700

    create Card component structure and style

--- projects/flash-card/src/libs/Card.svelte
+++ projects/flash-card/src/libs/Card.svelte
@@ -1,1 +1,37 @@
-<h1>card</h1>
+<div class="card">
+  <div class="test-board">
+    <p class="test-text question">
+      What is the purpose of the `any` type in TypeScript, and when should it be
+      used (and avoided)?
+    </p>
+  </div>
+</div>
+
+<style>
+  .card {
+    width: 100%;
+    height: 36rem;
+    background-color: #fff;
+    border: 1px solid black;
+    border-radius: var(--radius);
+    padding: 4rem;
+  }
+
+  .test-board {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .test-text {
+    font-size: 2rem;
+    text-align: center;
+    line-height: 1.5;
+
+    &.answer {
+      font-size: 1.5rem;
+    }
+  }
+</style>
