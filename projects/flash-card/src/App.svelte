f8edd0c9dde6d9f53cfa7ecea29fffaa7ddaaf3f
Altanfiz <altanfizgenity@gmail.com>
2025-02-26 10:38:17 +0700

    style flash-card container

--- projects/flash-card/src/App.svelte
+++ projects/flash-card/src/App.svelte
@@ -1,11 +1,26 @@
-<script>
+<script lang="ts">
+  import { onMount } from "svelte";
   import Card from "./libs/Card.svelte";
   import Controller from "./libs/Controller.svelte";
   import ProgressBar from "./libs/ProgressBar.svelte";
+  import Timer from "./libs/Timer.svelte";
 </script>
 
-<main id="flash-card">
+<main class="flash-card">
+  <Timer />
   <ProgressBar />
   <Card />
   <Controller />
 </main>
+
+<style>
+  .flash-card {
+    width: 1024px;
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+    align-items: center;
+    justify-content: center;
+    background-color: var(--background);
+  }
+</style>
