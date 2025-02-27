639399a88b86bf3807ab0ddbf1d4cb5d8a1b56db
Altanfiz <altanfizgenity@gmail.com>
2025-02-27 11:55:50 +0700

    add Test type

--- projects/flash-card/src/vite-env.d.ts
+++ projects/flash-card/src/vite-env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+type TestRaw = {
+  question: string;
+  answer: string;
+};
+
+type Test = TestRaw & {
+  id: string;
+  isOpen: boolean;
+};
